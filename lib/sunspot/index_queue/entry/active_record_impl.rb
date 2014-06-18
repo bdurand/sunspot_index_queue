bd21126f048e652c745f9bd04c82b658b10bda7d
Shaun Dern <shaun@substantial.com>
2014-06-17 20:35:38 -0400

    Support activerecord 4.1

--- lib/sunspot/index_queue/entry/active_record_impl.rb
+++ lib/sunspot/index_queue/entry/active_record_impl.rb
@@ -20,16 +20,16 @@ module Sunspot
       # as the primary key, you can add additional statements to the migration to do so.
       class ActiveRecordImpl < ActiveRecord::Base
         include Entry
-        
+
         self.table_name = "sunspot_index_queue_entries"
 
         class << self
           # Implementation of the total_count method.
           def total_count(queue)
             conditions = queue.class_names.empty? ? {} : {:record_class_name => queue.class_names}
-            count(:conditions => conditions)
+            where(conditions).count(:all)
           end
-          
+
           # Implementation of the ready_count method.
           def ready_count(queue)
             conditions = ["#{connection.quote_column_name('run_at')} <= ?", Time.now.utc]
@@ -37,7 +37,7 @@ module Sunspot
               conditions.first << " AND #{connection.quote_column_name('record_class_name')} IN (?)"
               conditions << queue.class_names
             end
-            count(:conditions => conditions)
+            where(conditions).count(:all)
           end
 
           # Implementation of the error_count method.
@@ -47,7 +47,7 @@ module Sunspot
               conditions.first << " AND #{connection.quote_column_name('record_class_name')} IN (?)"
               conditions << queue.class_names
             end
-            count(:conditions => conditions)
+            where(conditions).count(:all)
           end
 
           # Implementation of the errors method.
@@ -57,15 +57,15 @@ module Sunspot
               conditions.first << " AND #{connection.quote_column_name('record_class_name')} IN (?)"
               conditions << queue.class_names
             end
-            all(:conditions => conditions, :limit => limit, :offset => offset, :order => :id)
+            where(conditions).limit(limit).offset(offset).order(:id).to_a
           end
 
           # Implementation of the reset! method.
           def reset! (queue)
             conditions = queue.class_names.empty? ? {} : {:record_class_name => queue.class_names}
-            update_all({:run_at => Time.now.utc, :attempts => 0, :error => nil, :lock => nil}, conditions)
+            where(conditions).update_all(:run_at => Time.now.utc, :attempts => 0, :error => nil, :lock => nil)
           end
-          
+
           # Implementation of the next_batch! method.
           def next_batch!(queue)
             conditions = ["#{connection.quote_column_name('run_at')} <= ?", Time.now.utc]
@@ -73,29 +73,29 @@ module Sunspot
               conditions.first << " AND #{connection.quote_column_name('record_class_name')} IN (?)"
               conditions << queue.class_names
             end
-            batch_entries = all(:select => "id", :conditions => conditions, :limit => queue.batch_size, :order => 'priority DESC, run_at')
+            batch_entries = select(:id).where(conditions).limit(queue.batch_size).order('priority DESC, run_at')
             queue_entry_ids = batch_entries.collect{|entry| entry.id}
             return [] if queue_entry_ids.empty?
             lock = rand(0x7FFFFFFF)
-            update_all({:run_at => queue.retry_interval.from_now.utc, :lock => lock, :error => nil}, :id => queue_entry_ids)
-            all(:conditions => {:id => queue_entry_ids, :lock => lock})
+            where(:id => queue_entry_ids).update_all(:run_at => queue.retry_interval.from_now.utc, :lock => lock, :error => nil)
+            where(:id => queue_entry_ids, :lock => lock).to_a
           end
 
           # Implementation of the add method.
           def add(klass, id, delete, priority)
             queue_entry_key = {:record_id => id, :record_class_name => klass.name, :lock => nil}
-            queue_entry = first(:conditions => queue_entry_key) || new(queue_entry_key.merge(:priority => priority))
+            queue_entry = where(queue_entry_key).first || new(queue_entry_key.merge(:priority => priority))
             queue_entry.is_delete = delete
             queue_entry.priority = priority if priority > queue_entry.priority
             queue_entry.run_at = Time.now.utc
             queue_entry.save!
           end
-          
+
           # Implementation of the delete_entries method.
           def delete_entries(entries)
             delete_all(:id => entries)
           end
-          
+
           # Create the table used to store the queue in the database.
           def create_table
             connection.create_table table_name do |t|
